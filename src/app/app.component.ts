e20fc3f9d16c21249397094191f5909e990321af
Stefan Haas <stefan.haas.privat@gmail.com>
2023-12-17 22:36:21 +0100

    zoneless lotr character explorer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,28 @@ import { Router, RouterModule } from '@angular/router';
 import { CounterComponent } from './counter/counter.component';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { rxEffects } from '@rx-angular/state/effects';
-import { debounceTime, distinctUntilChanged, filter } from 'rxjs';
+import {
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  interval,
+  map,
+  mergeMap,
+  tap,
+} from 'rxjs';
+import { CharacterComponent } from './characters/character/character.component';
+import { RxPush } from '@rx-angular/template/push';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, CounterComponent, ReactiveFormsModule],
+  imports: [CharacterComponent, CounterComponent, ReactiveFormsModule, RxPush],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  readonly #router = inject(Router);
   readonly input = new FormControl('');
   readonly inputEvent$ = this.input.valueChanges.pipe(
     filter((value) => !!value && value.length > 0),
@@ -22,9 +32,14 @@ export class AppComponent {
     distinctUntilChanged()
   );
 
-  readonly effects = rxEffects(({ register }) => {
-    register(this.inputEvent$, (value) => {
-      this.#router.navigate(['character', value]);
-    });
-  });
+  readonly id$ = this.inputEvent$.pipe(
+    filter((value) => !isNaN(Number(value))),
+    map((value) => Number(value)),
+    tap((x) => console.log('id$ tap', x))
+  );
+
+  readonly diceTrigger = new Subject<void>();
+  readonly dice$ = this.diceTrigger
+    .asObservable()
+    .pipe(map(() => Math.floor(Math.random() * 10) + 1));
 }
